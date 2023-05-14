6f1529e93eb2264e35050c0aae5f43a5b1058654
Abhishek Venkatesh <me@defstryker.dev>
2023-05-14 21:39:36 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::{
+    env,
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+const SHA1_HASH_STRING_LENGTH: usize = 40;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = env::args().collect::<Vec<String>>();
+
+    if args.len() != 3 {
+        println!("Usage:");
+        println!("sha1_cracker <wordlist.txt> <sha1_hash>");
+        return Ok(()); 
+    }
+
+    // checking the validity of the hash
+    let hash_to_crack = args[2].trim();
+    if hash_to_crack.len() != SHA1_HASH_STRING_LENGTH {
+        return Err("SHA1 hash is invalid".into());
+    }
+
+    // opening the wordlist
+    let wordlist_file = File::open(&args[1])?;
+    let reader = BufReader::new(&wordlist_file);
+
+    // read lines
+    for line in reader.lines() {
+        let line = line?.trim().to_string();
+        println!("{}", line);
+    }
+
+    Ok(())
+}
